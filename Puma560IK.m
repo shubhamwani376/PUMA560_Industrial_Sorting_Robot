10a58473db6cb3c5cb28c337ca2e06972f97a0e5
Shubham Wani <45815852+shubhamwani376@users.noreply.github.com>
2022-05-25 12:23:32 -0700

    Update Puma560IK.m

--- Puma560IK.m
+++ Puma560IK.m
@@ -12,11 +12,11 @@ r11 = Tdes(1,1); r21 = Tdes(2,1); r31 = Tdes(3,1);
 r12 = Tdes(1,2); r22 = Tdes(2,2); r32 = Tdes(3,2);
 r13 = Tdes(1,3); r23 = Tdes(2,3); r33 = Tdes(3,3);
 
-t1 = atan2(y,x) - atan2(d3, sqrt(x^2 + y^2 - d3^2)); %Joint 1
+t1 = atan2(y,x) - atan2(d3, +1*sqrt(x^2 + y^2 - d3^2)); %Joint 1
 
 K = (x^2 + y^2 + z^2 - a2^2 - a3^2 - d3^2 - d4^2)/(2*a2);
 
-t3 = atan2(a3,d4) - atan2(K, sqrt(a3^2 + d4^2 - K^2)); %Joint 3
+t3 = atan2(a3,d4) - atan2(K, 1*sqrt(a3^2 + d4^2 - K^2)); %Joint 3
 
 t23 = atan2((-a3-a2*cos(t3))*z - (cos(t1)*x+sin(t1)*y)*(d4-a2*sin(t3)), z*(a2*sin(t3)-d4) + (a3+a2*cos(t3))*(cos(t1)*x+sin(t1)*y));
 
