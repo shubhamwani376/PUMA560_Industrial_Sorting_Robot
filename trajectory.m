c2a931978413294e534ddd79c34c5b9a609313e9
Shubham Wani <45815852+shubhamwani376@users.noreply.github.com>
2022-06-03 00:06:26 -0700

    changes

--- trajectory.m
+++ trajectory.m
@@ -1,5 +1,7 @@
 %axes 6, 5 points, 3 via points
-x_home=0.4115;y_home=0.1501;z_home=0.4331;
+function [x_d,dx_d,ddx_d,trajtime] = trajectory(T)
+
+x_home=T(1,4);y_home=T(2,4);z_home=T(3,4);
 
 q0=[x_home,y_home,z_home];
 wp=[ x_home+0.1,y_home,z_home;
@@ -8,7 +10,7 @@ wp=[ x_home+0.1,y_home,z_home;
     x_home,y_home,z_home];
 tseg = [10,10,10,10];
 tacc = tseg(1)/10;
-T = linspace(0,40,400);
+trajtime = linspace(0,40,400);
 x_d = mstraj(wp, [], tseg, q0, 0.1, tacc);
 dx_d=gradient(x_d);
 ddx_d=gradient(dx_d);
@@ -18,3 +20,5 @@ ddx_d=gradient(dx_d);
 % plot(T , dx_d(:,1))
 % figure(3)
 % plot(ddx_d(:,1),ddx_d(:,2))
+
+end
